b29fe90a2fc5118ed431db5db93343fe6822aacc
Krunal Astik <krunalastik7@gmail.com>
2022-10-08 18:41:22 +0530

    Carousel, Chart and Pdfviewer added

--- HazardousSubstancesBlazorServerApp/HazardousSubstancesBlazorServerApp/Model/Product.cs
+++ HazardousSubstancesBlazorServerApp/HazardousSubstancesBlazorServerApp/Model/Product.cs
@@ -5,11 +5,17 @@
         public int ID { get; set; }
         public string Name { get; set; }
 
-        List<string> Images { get; set; } = new List<string>();
+        public List<string> Images { get; set; } = new List<string>();
         public string Description { get; set; }
         public List<string> Warning { get; set; }
         public List<string> Info { get; set; }
-        
         public string Pdf { get; set; }
+        public List<HazardNessLevel> HazardNessLevels { get; set;}
+    }
+
+    public class HazardNessLevel
+    {
+        public string HazardNessName { get; set; }
+        public int Intensity { get; set; }
     }
 }
